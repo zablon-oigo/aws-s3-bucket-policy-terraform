16ab4922e5a11aa86edd62a06d36ebd637099ea3
zablon <zablonoigo51@gmail.com>
2025-03-25 22:05:36 +0300

    update variable list

--- var.tf
+++ var.tf
@@ -1,9 +1,3 @@
-variable "access_key" {
-  description = "Access key"
-}
-variable "secret_key" {
-  description = "Secret key"
-}
 variable "region" {
   description = "Region"
   default = "us-east-1"
