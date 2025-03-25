2daf3830ae585637a5ddfdd231df0739148ea944
zablon <zablonoigo51@gmail.com>
2025-03-25 20:53:55 +0300

    Add s3 lifecycle configuration

--- main.tf
+++ main.tf
@@ -22,3 +22,44 @@ resource "aws_s3_object" "obj" {
   key    = "foo.txt"
   source = "${path.module}/foo.txt"
 }
+
+
+resource "aws_s3_bucket_lifecycle_configuration" "rule" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  rule {
+    id     = "transition-to-one-zone-ia"
+    status = "Enabled"
+
+    filter {
+      prefix = ""
+    }
+
+    transition {
+      days          = 30
+      storage_class = "ONEZONE_IA"
+    }
+
+    expiration {
+      days = 120
+    }
+  }
+
+  rule {
+    id     = "transition-to-glacier"
+    status = "Enabled"
+
+    filter {
+      prefix = ""
+    }
+
+    transition {
+      days          = 90
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 120
+    }
+  }
+}
